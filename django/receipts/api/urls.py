c03650861f928c124f30d7b158296d7f20c836cd
alerr415 <alerr415@hotmail.com>
2023-04-26 21:27:01 +0200

    added view to get all retailers

--- django/receipts/api/urls.py
+++ django/receipts/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('receipts/', views.getReceipts),
     path('receipts/<str:pk>/', views.getReceiptDetails),
     path('receipts/retailer/<str:pk>/', views.getReceiptsByRetailer), # name of the retailer is the pk
+    path('retailers/', views.getRetailers),
 ]
